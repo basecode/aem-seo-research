c3e0a8d76a22f48443fd75f745aa8a1dc149164a
Tobias Reiss <reiss@adobe.com>
2025-02-17 10:05:23 +0100

    fix csv export

--- all-detect-pss-sites.js
+++ all-detect-pss-sites.js
@@ -26,20 +26,25 @@ if (!fs.existsSync(DIR)) {
 
 dotenv.config();
 
-(async function () {
+(async function main() {
   const sanitizeFilename = (url) => url.replace(/[^a-zA-Z0-9]/g, '_');
   const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
 
+  const fetchWithTimeout = (url, options, timeout = 10000) => Promise.race([
+    fetch(url, options),
+    new Promise((_, reject) => { setTimeout(() => reject(new Error('Timeout')), timeout); }),
+  ]);
+
   await Promise.all(sitesData.map(async (site) => {
-    // stop when we reach 10 sites
-    // if (sitesData.indexOf(site) > 1000) {
-    //  return;
-    // }
-
     // Ensure page is defined
-    if (!('finalUrl' in site) || !site.finalUrl) return;
-
-    const { finalUrl: page } = site;
+    let page;
+    let isFinalUrl = false;
+    if (!('finalUrl' in site) || !site.finalUrl) {
+      page = site.website;
+    } else {
+      isFinalUrl = true;
+      page = site.finalUrl;
+    }
 
     // read the cached file if it exists
     const filename = path.join(DIR, `${sanitizeFilename(page)}.txt`);
@@ -52,16 +57,10 @@ dotenv.config();
       // eslint-disable-next-line no-nested-ternary
       const deliveryType = isEDS ? 'EDS' : (isAEM ? 'AEM-CS' : '');
 
-      if (hasRUM || deliveryType) {
-        // console.log(`hasRUM: ${hasRUM}; deliveryType: ${deliveryType}; ${page}`);
-      }
-
-      if (isError) {
-        // console.error(`Error fetching ${page}`);
-      }
-
       // Update sitesData
-      const siteIndex = sitesData.findIndex((s) => s.finalUrl === page);
+      const siteIndex = (isFinalUrl)
+        ? sitesData.findIndex((s) => s.finalUrl === page)
+        : sitesData.findIndex((s) => s.website === page);
       if (siteIndex !== -1) {
         // eslint-disable-next-line no-nested-ternary
         if (isError) sitesData[siteIndex].status = text.includes('ERROR - ') ? text.replace('ERROR - ', '') : (text.includes('Incapsula') ? 'Incapsula blocker' : '');
@@ -75,7 +74,7 @@ dotenv.config();
     // fetch the page and cache the response
     try {
       if (!page.includes('https')) return;
-      const response = await fetch(page, {
+      const response = await fetchWithTimeout(page, {
         headers: {
           'User-Agent': userAgent,
           Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -84,19 +83,19 @@ dotenv.config();
         },
       });
       if (!response.ok) {
-        console.error(`Failed to fetch ${page}`);
+        fs.writeFileSync(path.join(DIR, `${sanitizeFilename(page)}.txt`), `ERROR - ${response.status} ${response.statusText}`);
       } else {
         const text = await response.text(); // Await the response text
         fs.writeFileSync(path.join(DIR, `${sanitizeFilename(page)}.txt`), text);
       }
     } catch (error) {
-      console.error(`Error fetching ${page}:`, error.message);
       fs.writeFileSync(path.join(DIR, `${sanitizeFilename(page)}.txt`), `ERROR - ${error.message}`);
     }
   }));
 
   // Write sitesData to sites-data-excel-output.txt. Format shall be CSV
-  let output = 'Customer Name,Country,Website,Final URL,Status,Batch,Delivery Type,Has RUM,IMS Org\n';
+  // Headers: 'Customer Name;Country;Website;Final URL;Status;Batch;Delivery Type;Has RUM;IMS Org;
+  let output = '';
   sitesData.forEach((site) => {
     output += `${site.customerName};${site.country};${site.website};${site.finalUrl};${site.status};${site.batch};${site.deliveryType};${site.hasRUM};${site.imsOrg}\n`;
   });
